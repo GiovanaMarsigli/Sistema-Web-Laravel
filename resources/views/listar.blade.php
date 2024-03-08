9e992d18706aa81774a02d8a467f7aecbeeffbea
GiovanaMarsigli <marsigligiovana@gmail.com>
2024-03-08 17:47:15 -0300

    .

---
+++ resources/views/listar.blade.php
@@ -0,0 +1,62 @@
+<DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <title>Sistema Web</title>
+    </head>
+    <body>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                    <nav class="navbar navbar-expand-lg bg-primary mb-4" data-bs-theme="dark">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="#" >PROJETO WEB</a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                             <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                             <a class="nav-link disabled" aria-disabled="true">Consultar</a>
+                                        </li>
+                                     </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="mb-3 fs-5">Cadastrar - Agendamento de Potenciais Clientes</p>
+                        <p class="mb-3">Sistema Utilizado para agendamento de serviços.</p>
+                    </div>          
+                </div>
+                <div class="container text-center text-bg-primary mb-4">
+                    <div class="row">
+                        <div class="col">
+                             Nome
+                        </div>
+                        <div class="col">
+                             Telefone
+                        </div>
+                        <div class="col">
+                             Origem
+                        </div>
+                        <div class="col">
+                             Data de Contato
+                        </div>
+                        <div class="col">
+                             Observação
+                        </div>
+                     </div>
+                </div>
+       </section>
+    </body>
+</html>
